452f1218b37d0902b5ec2de9ab7a9a5573876bff
github username <github email address>
2020-01-12 15:12:05 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,33 @@
 class Instructor
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+    end 
+    
+    def self.all
+        @@all 
+    end 
+
+    def pass_student(student, test_name)
+
+        test = BoatingTest.all.find{|bt| bt.student.name == student.name && bt.test_name == test_name}
+        if test 
+           test.status = "passed"
+           test
+        else 
+            BoatingTest.new(student, test_name, "passed", self)
+        end 
+    end 
+
+    def fail_student(student, test_name)
+        test = BoatingTest.all.find{|bt| bt.student.name == student.name && bt.test_name == test_name}
+        if test 
+           test.status == "failed"
+           test
+        else 
+            BoatingTest.new(student, test_name, "failed", self)
+        end 
+    end 
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,36 @@
 class Student
 
-end
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name 
+
+        @@all << self 
+    end 
+
+    def self.all
+        @@all 
+    end 
+
+    def self.find_student(name)
+        @@all.select{|student| student.name == self.name}
+    end 
+
+    def add_boating_test(test_name, status, instructor)
+        BoatingTest.new(self,test_name, status, instructor)
+    end 
+
+    def grade_percentage
+        my_tests = BoatingTest.all.select{|test| test.student == self}
+        total_tests = my_tests.count 
+        passed_tests = 0 
+        my_tests.each do |test|
+            if test.status == "passed"
+                passed_tests += 1 
+            end 
+        end 
+        (passed_tests.to_f/total_tests.to_f)
+    end 
+    
+
+end 
