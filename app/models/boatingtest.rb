9ad12dd09090472a5704021662f084267d0a3e49
Jorge Basilio <J.l.Basilio55@gmail.com>
2020-01-13 18:39:27 -0500

    Done.

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,22 @@
 class BoatingTest
 
+     attr_accessor :studet, :name, :boating_test, :instructor
+
+     @@all = []
+
+    def initialize(student, name, boating_test, instructor)
+        @student = student
+        @name = name
+        @boating_test = boating_test
+        @instructor = instructor
+
+        @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,42 @@
+require 'pry'
 class Student
 
+    attr_accessor :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+
+    end
+
+    def boatingtest
+        BoatingTest.all.select do |test|
+            test.student == self
+        end
+    end
+
+    def students
+        boatingtest.all.map do |test|
+            test.instructor == self
+        end
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test
+        BoatingTest.new("Don't Crash 101", "pending", puff)
+    end
+
+    def self.find_student
+        
+    end
 end
+
+
+
+binding.pry
+
