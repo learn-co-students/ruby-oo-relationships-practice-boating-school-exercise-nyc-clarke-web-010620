bf08fd68213dd06105206fcee1af89e656f54212
Leon Jamison <leonjamison@icloud.com>
2020-01-13 18:15:32 -0500

    Complete

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,20 @@
 class BoatingTest
+    attr_accessor :boating_test_status
+    attr_reader :student, :boating_test_name
+    @@all = []
+
+    def initialize(student, boating_test_name, boating_test_status, instructor)
+        @student = student 
+        @boating_test_name = boating_test_name
+        @boating_test_status = boating_test_status
+        @instructor = instructor
+        
+        @@all << self 
+    end 
+
+    def self.all 
+        @@all
+    end 
+
 
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,38 @@
 class Instructor
+    attr_accessor :boating_test_status
+    attr_reader :student 
+@@all = []
+
+    def initialize(name)
+        @name = name 
+
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def pass_student(student, test_name)
+        test = BoatingTest.all.find{|bt| bt.student.first_name == student.first_name && bt.boating_test_name == boating_test_name}
+        if test
+            test.boating_test_status = "passed"
+            test
+        else
+            BoatingTest.new(student, boat_test_name, 'passed')
+        end 
+
+    end 
+
+    def fail_student(student, boating_test_name, boating_test_status)
+        test = BoatingTest.all.find{|bt| bt.student.first_name == student.first_name && bt.boating_test_name == boating_test_name}
+        if test
+            test.boating_test_status = "failed"
+            test
+        else
+            BoatingTest.new(student, boat_test_name, 'failed')
+        end
+        
+    end 
 
 end
