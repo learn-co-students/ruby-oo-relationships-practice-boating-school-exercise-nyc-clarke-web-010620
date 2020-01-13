7d0abd49b1f8913fdd130f97f1b505c9a7a7b543
Granit <ghaxhaj@fordham.edu>
2020-01-13 15:44:20 -0500

    complete

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,39 @@
 class Instructor
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pass_student(student,test_name)
+        BoatingTest.all.each do |y|
+            if y.student == student && y.test_name == test_name
+                y.status=("passed")
+            else 
+            BoatingTest.new(student,test_name,self)
+            y.status=("passed")
+            end
+        end
+    end
+
+    def fail_student(student,test_name)
+        BoatingTest.all.each do |y|
+            if y.student == student && y.test_name == test_name
+                y.status=("failed")
+            else 
+                BoatingTest.new(student,test_name,self)
+                y.status=("failed")
+            end
+        end
+    end
+
 
 end
+   
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,39 @@
 class Student
+    attr_accessor :first_name
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name,instructor)
+        BoatingTest.new(self,test_name,instructor)
+    end
+
+    def self.find_student(first_name)
+        @@all.find do |y|
+            y.first_name == first_name
+        end
+    end
+
+    def my_tests
+        BoatingTest.all.select do |test|
+            test.student == self
+        end
+    end
+
+    def grade_percentage
+        passed = self.my_tests.select {|test| test.status == "passed"}
+        passed1 = passed.count.to_f
+        total = self.my_tests.count.to_f
+        passed1/total
+    end
 
 end
+
+
